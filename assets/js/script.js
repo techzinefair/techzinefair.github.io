4b391b6c2437059f32f4d1d35d93a0ffad47c93f
Cezar Mocan <mocancezar@gmail.com>
2018-11-25 17:41:17 -0500

    Header as include

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
     $(document).ready(function(){
         $(window).scroll(function(){
             console.log('Href: ', window.location)
-            if ($(this).scrollTop() > 350 || window.location.href == 'blog') {
+            if ($(this).scrollTop() > 350) {
+                console.log('Bringing in navbar')
                 $('#navbar').fadeIn(200);
             } else {
                 $('#navbar').fadeOut(200);
