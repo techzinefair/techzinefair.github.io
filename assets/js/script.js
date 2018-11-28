934c72c9bb2e5df8efe535b1558e78c4df62a80f
Cezar Mocan <mocancezar@gmail.com>
2018-11-28 01:32:55 -0500

    Removed debug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,7 @@
 (function($) {
     $(document).ready(function(){
         $(window).scroll(function(){
-            console.log('Href: ', window.location)
             if ($(this).scrollTop() > 350) {
-                console.log('Bringing in navbar')
                 $('#navbar').fadeIn(200);
             } else {
                 $('#navbar').fadeOut(200);
